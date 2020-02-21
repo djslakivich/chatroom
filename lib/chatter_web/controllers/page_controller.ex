b7ec97b5d696834c891565d67ffa3afaa72d6263
Mohammad <mo3greentv@gmail.com>
2020-02-21 17:31:11 -0500

    first chatroom

--- lib/chatter_web/controllers/page_controller.ex
+++ lib/chatter_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule ChatterWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def chatone(conn, _params) do
+    render(conn, "index.html")
+  end
+
 end
--- lib/chatter_web/router.ex
+++ lib/chatter_web/router.ex
@@ -17,6 +17,7 @@ defmodule ChatterWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/chatone", PageController, :chatone
   end
 
   # Other scopes may use custom stacks.
